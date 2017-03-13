4480d2f8d86c1f67c44a7a2c423be96d2048a282
Jonathan Silva <jteccondeuba@hotmail.com>
2017-03-13 11:27:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-    //define('__DIR_SESSION__', dirname('session/'));
-	const __DIR_SESSION__ = __DIR__;
+    define('DIR_SESSION', __DIR__);
     define('DIR', dirname(__FILE__));
     define('DS', DIRECTORY_SEPARATOR);
 	date_default_timezone_set('America/Sao_Paulo');
